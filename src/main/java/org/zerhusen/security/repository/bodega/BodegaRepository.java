0175f6781f0a3bc69b4eba7391b5b757c36defc5
Ferenc <Ferenc@192.168.0.106>
2018-06-11 00:42:08 -0400

    corregida consulta de usuario a empresa cliente

--- src/main/java/org/zerhusen/security/repository/bodega/BodegaRepository.java
+++ src/main/java/org/zerhusen/security/repository/bodega/BodegaRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 //Interfaz de la base de datos con el controllador Bodega
 public interface BodegaRepository extends JpaRepository<Bodega, Long>{
 	
-    @Query(value = "select * from bodega where id_e_cliente = "
-            + "(SELECT e_cliente.rut FROM user, e_cliente WHERE user.id = ?1)", nativeQuery = true)
+    @Query(value = "select * from bodega where id_e_cliente = any"
+            + "(select rut from e_cliente where rut = any "
+            + "(select id_e_cliente from funcionario where rut = any "
+            + "(select id_funcionario from user where id = ?1)))", nativeQuery = true)
     List<Bodega> listaTodo(long id);
     
 }
